3ee06bcb6aeae67807e0973fb239f5ea5d17692f
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-07-08 11:47:51 -0300

    Chore: Setting-up Prisma ORM

--- src/env.ts
+++ src/env.ts
@@ -1,13 +1,16 @@
-import {z} from 'zod'
+import { z } from 'zod'
 import 'dotenv/config'
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333)
+  PORT: z.coerce.number().default(3333),
+  NODE_ENV: z
+    .enum(['development', 'test', 'production'])
+    .default('development'),
 })
 
 const _env = envSchema.safeParse(process.env)
 
-if(_env.success === false){
+if (_env.success === false) {
   console.log('Invalid environment variables', _env.error.format())
 
   throw new Error('Invalid environment variables')
