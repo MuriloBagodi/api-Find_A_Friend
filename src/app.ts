70992d1cc6a2a32d2f9ddf809ec2916237ad7a29
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-07-16 11:56:27 -0300

    Register of fastifyJwtToken on app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,40 @@
 import fastify from 'fastify'
 import { orgsRoutes } from './http/controllers/Orgs/orgs.routes'
 import { petsRoutes } from './http/controllers/Pets/pets.routes'
+import fastifyJwt from '@fastify/jwt'
+import { env } from './env'
+import fastifyCookie from '@fastify/cookie'
+import { ZodError } from 'zod/lib/ZodError'
 
 export const app = fastify()
 
+app.register(fastifyJwt, {
+  secret: env.JWT_SECRET,
+  cookie: {
+    cookieName: 'refreshToken',
+    signed: false,
+  },
+  sign: {
+    expiresIn: '10m',
+  },
+})
+
+app.register(fastifyCookie)
+
 app.register(orgsRoutes)
 app.register(petsRoutes)
+
+app.setErrorHandler((error, _, res) => {
+  if (error instanceof ZodError) {
+    return res
+      .status(400)
+      .send({ message: 'Validation error', issues: error.format() })
+  }
+
+  if (env.NODE_ENV !== 'production') {
+    console.error(error)
+  } else {
+    // TODO: Here we should log to an external tool like DataDog/New Relic/Sentry
+  }
+  return res.status(500).send({ message: 'Internal Server Error' })
+})
