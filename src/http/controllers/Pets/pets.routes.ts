a2bd4330ed6f6aabb909826498fc954be6b60ddc
Murilo Bagodi <murilosbagodi@hotmail.com>
2024-07-16 14:58:04 -0300

    Chore: add authentication barrer on creating pets

--- src/http/controllers/Pets/pets.routes.ts
+++ src/http/controllers/Pets/pets.routes.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from 'fastify'
 import { createPetController } from './create-pet.controller'
+import { verifyJWT } from '@/http/middleware/verifyJwt'
 
 export async function petsRoutes(app: FastifyInstance) {
-  app.post('/pets', createPetController)
+  app.post('/pets', { onRequest: [verifyJWT] }, createPetController)
 }
